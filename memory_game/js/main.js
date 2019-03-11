7d63f46c930b816f0c7c8dab771cf46ed0f07cc2
Clayton Smith <claytonsmith@Claytons-MacBook-Pro.local>
2019-03-11 03:48:43 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,20 @@
-console.log("Up and running!");
-
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!');
-	} else {
-		alert('Sorry, try again.');
-	}
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
+
+var flipCard = function (cardId) {
+  checkForMatch();
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+
+flipCard(2);
